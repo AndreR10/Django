e440b209e53f2542fbbb8bd9eef8bf467f566001
AndreR10 <andrem.tramos@gmail.com>
2020-10-06 22:19:27 +0100

    Created blog app templates for home and about pages

--- project/blog/views.py
+++ project/blog/views.py
@@ -2,11 +2,26 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
+posts = [
+    {
+        "author": "André Ramos",
+        "title": "Blog post 1",
+        "content": "Blabla",
+        "date_posted": "November 10, 2020",
+    },
+    {
+        "author": "Inês Serra",
+        "title": "Blog post 2",
+        "content": "Hahahah",
+        "date_posted": "November 8, 2020",
+    },
+]
 
 
 def home(request):
-    return HttpResponse("<h1> Blog Home </h1>")
+    context = {"posts": posts}
+    return render(request, "blog/home.html", context)
 
 
 def about(request):
-    return HttpResponse("<h1> Blog About </h1>")
+    return render(request, "blog/about.html", {"title": "About"})
